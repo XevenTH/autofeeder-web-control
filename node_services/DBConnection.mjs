bb99e3cba7cbb6d5f835b042ea5a3b0a34c2052f
XevenTH <emmanuelangga@gmail.com>
2024-06-10 04:48:17 +0700

    Fixed MQTT Server

--- node_services/DBConnection.mjs
+++ node_services/DBConnection.mjs
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 const config = {
   host: 'localhost',
-  user: '',
+  user: 'root',
   password: '',
   database: 'autofeeder'
 };
@@ -17,27 +17,85 @@ const ConnectDB = async () => {
   }
 };
 
-const FetchData = async () => {
-    try {
-      const connection = await ConnectDB();
+const GetSchedulesAll = async () => {
+  try {
+    const connection = await ConnectDB();
 
-      if (connection) {
-        const [rows] = await connection.execute('SELECT * FROM Jadwal');
-  
-        const resultArray = rows.map(row => ({
-          field1: row.field1,
-          field2: row.field2,
-        }));
-  
-        console.log('Data:', resultArray);
+    if (connection) {
+      const [rows] = await connection.execute('SELECT * FROM schedules');
 
-        await connection.end();
-  
-        return resultArray;
-      }
-    } catch (err) {
-      console.error('Error fetching data:', err);
+      const resultArray = rows.map(row => ({
+        id: row.id, // int
+        device_id: row.device_id, // int
+        active: row.active, // bool
+        days: row.days, // String
+        time: row.time, // String
+        grams_per_feeding: row.grams_per_feeding,  // int
+        servo_seconds: row.servo_seconds, // int
+      }));
+
+      await connection.end();
+
+      return resultArray;
     }
-  };
+  } catch (err) {
+    console.error('Error fetching data:', err);
+  }
+};
 
-export default FetchData;
+const GetDeviceAll = async () => {
+  try {
+    const connection = await ConnectDB();
+
+    if (connection) {
+      const [rows] = await connection.execute('SELECT * FROM devices');
+
+      const resultArray = rows.map(row => ({
+        id: row.id, // int
+        user_id: row.user_id, // int
+        name: row.name,
+        topic: row.topic,
+        capacity: row.capacity,
+      }));
+
+      await connection.end();
+
+      return resultArray;
+    }
+  } catch (err) {
+    console.error('Error fetching data:', err);
+  }
+}
+
+const GetSpecificDevice = async (id_device) => {
+  const connection = await ConnectDB();
+
+  try {
+    if (connection) {
+      await connection.execute('SELECT * FROM devices WHERE ');
+    }
+  } catch (error) {
+    console.error('Error Fetching Data: ', err);
+  }
+}
+
+const UpdateDeviceCapacity = async (capacity, id_device) => {
+  try {
+    const connection = await ConnectDB();
+
+    if (connection) {
+      await connection.execute(
+        'UPDATE devices SET capacity = ? WHERE id = ?',
+        [capacity, id_device]
+      );
+
+      await connection.end();
+
+      return resultArray;
+    }
+  } catch (err) {
+    console.error('Error fetching data:', err);
+  }
+}
+
+export { GetSchedulesAll, UpdateDeviceCapacity, GetDeviceAll };
