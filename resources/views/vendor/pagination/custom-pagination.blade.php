58c4e59097315cb78fce2843c574408af99bf5e7
pandragama <pan2rafa@gmail.com>
2024-12-04 12:25:07 +0700

    update: first and last page pagination

--- resources/views/vendor/pagination/custom-pagination.blade.php
+++ resources/views/vendor/pagination/custom-pagination.blade.php
@@ -8,6 +8,17 @@
                 <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
             @endif
 
+            {{-- First Page Link --}}
+            @if ($paginator->lastPage() > 5)
+                @if ($paginator->currentPage() > 3)
+                    <li><a href="{{ $paginator->url(1) }}" class="first-page">First</a></li>
+                @endif
+
+                @if ($paginator->currentPage() > 4)
+                    <li class="disabled"><span>...</span></li>
+                @endif
+            @endif
+
             {{-- Pagination Elements --}}
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
@@ -21,11 +32,24 @@
                         @if ($page == $paginator->currentPage())
                             <li class="active"><span>{{ $page }}</span></li>
                         @else
-                            <li><a href="{{ $url }}">{{ $page }}</a></li>
+                            @if ($page >= $paginator->currentPage() - 1 && $page <= $paginator->currentPage() + 1)
+                                <li><a href="{{ $url }}">{{ $page }}</a></li>
+                            @endif
                         @endif
                     @endforeach
                 @endif
             @endforeach
+
+            {{-- Last Page Link --}}
+            @if ($paginator->lastPage() > 5)
+                @if ($paginator->currentPage() < $paginator->lastPage() - 3)
+                    <li class="disabled"><span>...</span></li>
+                @endif
+
+                @if ($paginator->currentPage() < $paginator->lastPage() - 2)
+                    <li><a href="{{ $paginator->url($paginator->lastPage()) }}" class="last-page">Last</a></li>
+                @endif
+            @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
