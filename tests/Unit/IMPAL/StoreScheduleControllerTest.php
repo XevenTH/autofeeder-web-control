6680036990463dbf568b6c3a8e590fc5697cfc42
pandragama <pan2rafa@gmail.com>
2024-12-30 01:51:28 +0700

    add: unit test impal 1

---
+++ tests/Unit/IMPAL/StoreScheduleControllerTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Tests\Unit\IMPAL;
+
+use App\Models\User;
+use App\Models\Device;
+use App\Models\Schedule;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
+use Mockery;
+
+class StoreScheduleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+    protected $device;
+    protected $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        // Membuat pengguna untuk pengujian
+        // Periksa ketersediaan akun tester
+        $tester = User::where('email', 'admin@finbites.com')->first();
+        if ($tester == null) {
+            // Membuat pengguna dengan email khusus tester
+            $this->user = User::factory()->create([
+                'name' => 'Tester',
+                'email' => 'admin@finbites.com',
+            ]); 
+        } else {
+            $this->user = $tester;
+        }
+        $this->device = Device::factory()->create(['user_id' => $this->user->id]);
+        $this->actingAs($this->user);
+
+        // Mocking GuzzleHttp Client
+        $this->client = Mockery::mock(Client::class);
+        $this->app->instance(Client::class, $this->client);
+        $this->startSession(); // Ensure the session is started
+    }
+
+    public function test_store_invalid_input()
+    {
+        // Debugging: Check session data
+        // $sessionData = session()->all();
+        // Log::info('Session Data store 1:', $sessionData);
+        
+        // Menggunakan CSRF token dalam header
+        $response = $this->withHeaders([
+            'X-CSRF-TOKEN' => csrf_token(),
+        ])->postJson('/schedules/admin', [
+            'device_id' => 99,
+            'time' => '',
+            'grams_per_feeding' => 0,
+            'active' => 1,
+        ]);
+
+        $response->assertStatus(422); // Uprocessable Entity, tidak memenuhi kriteria validasi yang ditentukan oleh server
+        $response->assertJsonValidationErrors(['device_id', 'time', 'grams_per_feeding']);
+    }
+
+    public function test_store_valid_input_no_days()
+    {
+        // Menggunakan CSRF token dalam header
+        $response = $this->withHeaders([
+            'X-CSRF-TOKEN' => csrf_token(),
+        ])->postJson('/schedules/admin', [
+            'device_id' => $this->device->id,
+            'time' => '12:00',
+            'grams_per_feeding' => 60,
+            'active' => 1,
+        ]);
+
+        $response->assertStatus(302); // Resource yang di-request telah dipindahkan sementara ke lokasi baru (permintaan HTTP berhasil)
+        $this->assertDatabaseHas('schedules', [
+            'device_id' => $this->device->id,
+            'time' => '12:00',
+            'grams_per_feeding' => 60,
+            'days' => '-', // Tidak ada hari yang dipilih
+        ]);
+    }
+
+    public function test_store_valid_input_with_days()
+    {
+        // Menggunakan CSRF token dalam header
+        $response = $this->withHeaders([
+            'X-CSRF-TOKEN' => csrf_token(),
+        ])->postJson('/schedules/admin', [
+            'device_id' => $this->device->id,
+            'time' => '12:00',
+            'grams_per_feeding' => 60,
+            'active' => 1,
+            'days_monday' => 'Monday',
+            'days_wednesday' => 'Wednesday',
+        ]);
+
+        $response->assertStatus(302); // Resource yang di-request telah dipindahkan sementara ke lokasi baru (permintaan HTTP berhasil)
+        $this->assertDatabaseHas('schedules', [
+            'device_id' => $this->device->id,
+            'time' => '12:00',
+            'grams_per_feeding' => 60,
+            'days' => 'Monday Wednesday ', // Hari yang dipilih
+        ]);
+
+    }
+
+    public function test_store_valid_input_refresh_failed()
+    {
+        $this->client->shouldReceive('request')
+            ->once()
+            ->with('POST', 'http://localhost:3000/api/refresh')
+            ->andThrow(new RequestException("Error Communicating with Server", new \GuzzleHttp\Psr7\Request('POST', 'http://localhost:3000/api/refresh')));
+
+        // Menggunakan CSRF token dalam header
+        $response = $this->withHeaders([
+            'X-CSRF-TOKEN' => csrf_token(),
+        ])->postJson('/schedules/admin', [
+            'device_id' => $this->device->id,
+            'time' => '12:00',
+            'grams_per_feeding' => 60,
+            'active' => 1,
+            'days_monday' => 'Monday',
+        ]);
+
+        $response->assertStatus(302); // Resource yang di-request telah dipindahkan sementara ke lokasi baru (permintaan HTTP berhasil)
+        $response->assertSessionHas('toast_error', "Gagal menyegarkan jadwal di server: Error Communicating with Server");
+    }
+
+    // public function test_store_valid_input_database_save_failed()
+    // {
+    //     // Mock database failure
+    //     Schedule::saving(function () {
+    //         throw new \Exception('Database error');
+    //     });
+
+    //     $response = $this->withHeaders([
+    //         'X-CSRF-TOKEN' => csrf_token(),
+    //     ])->postJson('/schedules/admin', [
+    //         'device_id' => $this->device->id,
+    //         'time' => '12:00',
+    //         'grams_per_feeding' => 60,
+    //         'active' => 1,
+    //     ]);
+
+    //     $response->assertStatus(500);
+    // }
+}
